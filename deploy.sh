d7496f3cc6428d8126652e9661ca033b26959098
asalia0316 <asalia0316@qq.com>
2025-11-27 23:33:04 +0800

    1

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run docs:build
+
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:<用户名>/<仓库名>.git master:gh-pages
+
+cd -
